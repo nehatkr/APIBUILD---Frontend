7f4ba91542dc188530534c3be2bfaf49f7961ad4
Nehatkr <nehathakur2308@gmail.com>
2025-11-03 16:40:15 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-
-import './index.css'
-import Login from "./components/Login"
+import "./index.css";
+import Login from "./components/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
 
 function App() {
-
   return (
     <>
-<Login />
+    <BrowserRouter basename="/">
+    <Routes>
+      <Route path="/" element={<Body />}>
+      <Route path="/login" element={<Login />}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  
     </>
-  )
+  );
 }
 
-export default App
+export default App;
